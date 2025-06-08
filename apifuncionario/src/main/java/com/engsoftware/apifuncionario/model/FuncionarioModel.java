2614d148e3b46bfdb8a03e108c8eaf4aee1ab051
MarcusViniLima <marcus12367lima@gmail.com>
2025-06-07 21:41:09 -0300

    apiFuncionario: configurando resource de testes

--- apifuncionario/src/main/java/com/engsoftware/apifuncionario/model/FuncionarioModel.java
+++ apifuncionario/src/main/java/com/engsoftware/apifuncionario/model/FuncionarioModel.java
@@ -10,10 +10,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FuncionarioModel {
 
     @Id
---
+++ apifuncionario/src/test/java/com/engsoftware/apifuncionario/FuncionarioServiceTest.java
@@ -0,0 +1,15 @@
+package com.engsoftware.apifuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+public class FuncionarioServiceTest
+ {
+
+    @Test
+    void ok() {
+        
+    }
+
+}
