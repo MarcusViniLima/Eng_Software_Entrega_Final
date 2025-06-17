903bca6f6cc3e721cb12193497b811d77a83b072
MarcusViniLima <marcus12367lima@gmail.com>
2025-06-17 20:33:59 -0300

    apiemail: iniciando projeto

---
+++ apiemail/src/main/java/com/engsoftware/apiemail/ApiemailApplication.java
@@ -0,0 +1,13 @@
+package com.engsoftware.apiemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiemailApplication.class, args);
+	}
+
+}
---
+++ apiemail/src/test/java/com/engsoftware/apiemail/ApiemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoftware.apiemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
